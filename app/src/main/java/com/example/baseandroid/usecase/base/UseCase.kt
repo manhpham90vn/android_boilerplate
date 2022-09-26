d96bb82be7cdb2857c736f89582fedf2f1591277
Manh Pham (OFA iOS) <manhpham90vn@icloud.com>
2022-09-26 17:33:29 +0700

    refactor (#45)

--- app/src/main/java/com/example/baseandroid/usecase/base/UseCase.kt
+++ app/src/main/java/com/example/baseandroid/usecase/base/UseCase.kt
@@ -2,5 +2,7 @@ package com.example.baseandroid.usecase.base
 
 abstract class UseCase<in P, out R> {
     abstract fun buildUseCase(params: P): R
-    abstract fun execute(params: P)
+    open fun execute(params: P) {
+        buildUseCase(params)
+    }
 }
