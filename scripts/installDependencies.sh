3e2e966ca0458a9bcc58b6b199418532853106bc
Manh Pham <manhpham90vn@icloud.com>
2022-12-08 11:02:09 +0700

    fix ci

---
+++ scripts/installDependencies.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+if ! rbenv -v &> /dev/null
+then
+    brew install rbenv
+fi
+
+if ! bundle --version &> /dev/null
+then
+    export BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d " ")
+    gem install bundler:$BUNDLER_VERSION
+fi
+
+echo "Done"
