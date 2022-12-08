5dc01816fe409faeb2fbfee1f0d223f4bc68618e
Manh Pham <manhpham90vn@icloud.com>
2022-12-08 11:06:57 +0700

    fix

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -5,6 +5,7 @@ if ! bundle --version &> /dev/null
 then
     export BUNDLER_VERSION=$(cat Gemfile.lock | tail -1 | tr -d " ")
     sudo gem install bundler:$BUNDLER_VERSION
+    bundle install
 fi
 
 echo "Done"
